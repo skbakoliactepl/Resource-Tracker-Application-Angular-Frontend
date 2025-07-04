b12649e9f2c47a64be69e1cf4874fb2157db886b
sakoliactepl <sbakoliactepl@gmail.com>
2025-07-04 13:02:52 +0530

    feat: add export option button

--- src/app/components/resource-grid/resource-grid.component.ts
+++ src/app/components/resource-grid/resource-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { GridModule } from '@progress/kendo-angular-grid';
 import { Resource } from '../../models/resource.model';
 import { MatIconModule } from '@angular/material/icon';
@@ -6,24 +6,65 @@ import { Router, RouterModule } from '@angular/router';
 import { ResourceService } from '../../services/resource.service';
 import { DialogModule } from '@progress/kendo-angular-dialog';
 import { CommonModule } from '@angular/common';
+import { filePdfIcon, fileExcelIcon, fileCsvIcon, dataJsonIcon } from "@progress/kendo-svg-icons";
+import { KENDO_BUTTONS } from "@progress/kendo-angular-buttons";
+import { KENDO_ICON, KENDO_ICONS } from "@progress/kendo-angular-icons";
 
 @Component({
   selector: 'app-resource-grid',
   templateUrl: './resource-grid.component.html',
   styleUrls: ['./resource-grid.component.css'],
   standalone: true,
-  imports: [CommonModule, GridModule, MatIconModule, RouterModule, DialogModule],
+  imports: [CommonModule, GridModule, MatIconModule, RouterModule, DialogModule, KENDO_BUTTONS, KENDO_ICONS],
+  styles: [`
+      .export-btn-group {
+        font-size: 12px;
+        font-weight: 400;
+        border-radius: var(--button-radius);
+        transition: all 0.3s ease;
+        color: white;
+        box-shadow: 0 0 0 transparent;
+        border: 2px solid var(--primary-color);
+        background-color: var(--primary-color);
+      } 
+      .export-btn-group:hover {
+        transform: scale(1.05);
+        box-shadow: 0 6px 14px rgba(197, 98, 175, 0.4);
+        background-color: var(--primary-color-dark);
+        border: 2px solid var(--primary-color-dark);
+      }
+    `]
 })
+
 export class ResourceGridComponent {
   resources: Resource[] = [];
+  showConfirmDialog: boolean = false;
+  selectedResource?: Resource;
+  resourceToDelete?: Resource;
+  public exportOptions = [
+    {
+      text: "Export to PDF",
+      svgIcon: filePdfIcon,
+      action: () => this.exportToPDF()
+    },
+    {
+      text: "Export to CSV",
+      svgIcon: fileCsvIcon,
+      action: () => this.exportToCSV()
+    },
+    {
+      text: "Export to JSON",
+      svgIcon: dataJsonIcon,
+      action: () => this.exportToJSON()
+    }
+  ];
+
+
   constructor(
     private resourceService: ResourceService,
     private router: Router
   ) { }
 
-  showConfirmDialog: boolean = false;
-  selectedResource?: Resource;
-  resourceToDelete?: Resource;
 
   ngOnInit() {
     this.loadResources();
@@ -76,6 +117,13 @@ export class ResourceGridComponent {
     this.router.navigate([`/resource-detail/${resource.empId}`]);
   };
 
+
+  onExportOptionSelected(option: any): void {
+    if (option?.action) {
+      option.action();
+    }
+  };
+
   exportToCSV(): void {
     const csvData = this.convertToCSV(this.resources);
     const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
@@ -86,6 +134,9 @@ export class ResourceGridComponent {
     a.setAttribute('download', `resources_${new Date().toString()}.csv`);
     a.click();
   };
+
+  exportToPDF(): void { };
+  exportToJSON(): void { };
 
   private convertToCSV(data: Resource[]): string {
     if (!data || data.length === 0) return '';
