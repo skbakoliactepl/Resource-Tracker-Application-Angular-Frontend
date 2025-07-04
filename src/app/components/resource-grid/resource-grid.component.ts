18d79d481eb3b759b622fc6b6c08fd8812e45d22
sakoliactepl <sbakoliactepl@gmail.com>
2025-07-04 13:20:40 +0530

    feat: add export to pdf feature

--- src/app/components/resource-grid/resource-grid.component.ts
+++ src/app/components/resource-grid/resource-grid.component.ts
@@ -1,21 +1,33 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { GridModule } from '@progress/kendo-angular-grid';
+import { Component, ViewChild } from '@angular/core';
+import { GridComponent, GridModule } from '@progress/kendo-angular-grid';
 import { Resource } from '../../models/resource.model';
 import { MatIconModule } from '@angular/material/icon';
 import { Router, RouterModule } from '@angular/router';
 import { ResourceService } from '../../services/resource.service';
 import { DialogModule } from '@progress/kendo-angular-dialog';
 import { CommonModule } from '@angular/common';
-import { filePdfIcon, fileExcelIcon, fileCsvIcon, dataJsonIcon } from "@progress/kendo-svg-icons";
+import { filePdfIcon, fileCsvIcon, dataJsonIcon, SVGIcon } from "@progress/kendo-svg-icons";
 import { KENDO_BUTTONS } from "@progress/kendo-angular-buttons";
-import { KENDO_ICON, KENDO_ICONS } from "@progress/kendo-angular-icons";
+import { KENDO_ICONS } from "@progress/kendo-angular-icons";
+import { PDFExportComponent } from '@progress/kendo-angular-pdf-export';
+import {
+  KENDO_GRID,
+  KENDO_GRID_PDF_EXPORT,
+} from "@progress/kendo-angular-grid";
+import { pdf } from '@progress/kendo-drawing';
+
+type ExportOption = {
+  text: string;
+  svgIcon: SVGIcon,
+  action: () => void;
+};
 
 @Component({
   selector: 'app-resource-grid',
   templateUrl: './resource-grid.component.html',
   styleUrls: ['./resource-grid.component.css'],
   standalone: true,
-  imports: [CommonModule, GridModule, MatIconModule, RouterModule, DialogModule, KENDO_BUTTONS, KENDO_ICONS],
+  imports: [CommonModule, GridModule, MatIconModule, RouterModule, DialogModule, KENDO_BUTTONS, KENDO_ICONS, KENDO_GRID_PDF_EXPORT],
   styles: [`
       .export-btn-group {
         font-size: 12px;
@@ -37,15 +49,18 @@ import { KENDO_ICON, KENDO_ICONS } from "@progress/kendo-angular-icons";
 })
 
 export class ResourceGridComponent {
+  @ViewChild(GridComponent) grid!: GridComponent;
+  pdfExport!: PDFExportComponent;
+
   resources: Resource[] = [];
   showConfirmDialog: boolean = false;
   selectedResource?: Resource;
   resourceToDelete?: Resource;
-  public exportOptions = [
+  public exportOptions: ExportOption[] = [
     {
       text: "Export to PDF",
       svgIcon: filePdfIcon,
-      action: () => this.exportToPDF()
+      action: () => this.grid.saveAsPDF()
     },
     {
       text: "Export to CSV",
@@ -118,11 +133,11 @@ export class ResourceGridComponent {
   };
 
 
-  onExportOptionSelected(option: any): void {
-    if (option?.action) {
-      option.action();
-    }
+  getPdfName(): string {
+    const pdfName = `resource_list_${new Date().toString()}`;
+    return pdfName;
   };
+
 
   exportToCSV(): void {
     const csvData = this.convertToCSV(this.resources);
@@ -131,12 +146,25 @@ export class ResourceGridComponent {
 
     const a = document.createElement('a');
     a.setAttribute('href', url);
-    a.setAttribute('download', `resources_${new Date().toString()}.csv`);
+    a.setAttribute('download', `resources_${new Date().toString()
+      }.csv`);
     a.click();
   };
 
   exportToPDF(): void { };
-  exportToJSON(): void { };
+  exportToJSON(): void {
+    const jsonData = JSON.stringify(this.resources, null, 2);
+
+    const blob = new Blob([jsonData], { type: 'application/json' });
+    const url = window.URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `resource - list_${new Date().toString()}.json`;
+    a.click();
+
+    window.URL.revokeObjectURL(url);
+  };
 
   private convertToCSV(data: Resource[]): string {
     if (!data || data.length === 0) return '';
