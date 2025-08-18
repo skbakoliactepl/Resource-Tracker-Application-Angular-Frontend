0486f2144453929b2b080368dee7d2a5f5a1e82c
Saurabh Bakolia <saurabh.bakolia@non.se.com>
2025-08-19 02:40:22 +0530

    chore: prod api string add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 // environment.prod.ts
 export const environment = {
     production: true,
-    apiBaseUrl: 'https://resource-tracker-api-ajcxauhmgwhadwhx.canadacentral-01.azurewebsites.net/api/Resource',
-    apiAuthUrl: 'https://resource-tracker-api-ajcxauhmgwhadwhx.canadacentral-01.azurewebsites.net/api/Auth',
-    apiAdminUrl: 'https://resource-tracker-api-ajcxauhmgwhadwhx.canadacentral-01.azurewebsites.net/api/Admin',
-
+    apiBaseUrl: 'resourcetrackerapi.azurewebsites.net/api/Resource',
+    apiAuthUrl: 'resourcetrackerapi.azurewebsites.net/api/Auth',
+    apiAdminUrl: 'resourcetrackerapi.azurewebsites.net/api/Admin',
 };
